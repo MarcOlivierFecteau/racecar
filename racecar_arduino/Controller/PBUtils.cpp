a01a3e2049f922cd84c3b40404414a998f303697
MarcOlivierFecteau <fecm0701@usherbrooke.ca>
2024-09-21 17:27:55 -0400

    Reformat code

--- racecar_arduino/Controller/PBUtils.cpp
+++ racecar_arduino/Controller/PBUtils.cpp
@@ -9,17 +9,15 @@ Date: Jan 2024
  * Constructor of the class
  * 
  * @param topics: Array of all the topics containing their type
- */ 
-PBUtils::PBUtils(const Topic *topics)
-{
-  for (int i = 0; i < _NBS_TOPICS; ++i)
-  {
+ */
+PBUtils::PBUtils(const Topic *topics) {
+  for (int i = 0; i < _NBS_TOPICS; ++i) {
     idToType[topics[i].id] = topics[i].type;
     idToMsg[topics[i].id] = topics[i].msg;
   }
 }
 
-PBUtils::~PBUtils() { }
+PBUtils::~PBUtils() {}
 
 /*
  * Convert a string to a list of PB messages
@@ -30,24 +28,22 @@ PBUtils::~PBUtils() { }
  * 
  * @return: if the decode was successful
  */
-bool PBUtils::decodePb(char* inputString, int *subMsgId, int &nbsNewMsgs)
-{
+bool PBUtils::decodePb(char *inputString, int *subMsgId, int &nbsNewMsgs) {
   char *subMsgs[MAX_MSG_LEN];
   bool success = true;
-  
+
   nbsNewMsgs = parseMsg(inputString, subMsgId, subMsgs);
 
   // Add assert nbs_new_msgs < length of sub_msg_id
 
-  for (int i = 0; i < nbsNewMsgs; ++i)
-  {
+  for (int i = 0; i < nbsNewMsgs; ++i) {
     uint8_t bufferIn[MAX_MSG_LEN];
     charsToBytes(subMsgs[i], bufferIn);
 
-    pb_istream_t streamIn = pb_istream_from_buffer(bufferIn, strlen(subMsgs[i])/2);
+    pb_istream_t streamIn = pb_istream_from_buffer(bufferIn, strlen(subMsgs[i]) / 2);
     success = success && pb_decode(&streamIn, idToType[subMsgId[i]], idToMsg[subMsgId[i]]);
   }
-  
+
   return success;
 }
 
@@ -58,33 +54,29 @@ bool PBUtils::decodePb(char* inputString, int *subMsgId, int &nbsNewMsgs)
  * @param ...: List of all the ids to send 
  *             ex : cant use it like this pb_send(3, POS, OBS_POS, DEBUG_ARDUINO);
  */
-void PBUtils::pbSend(int nbs, ...)
-{
+void PBUtils::pbSend(int nbs, ...) {
   bool success = true;
   String toSendBuilder = "<";
   va_list idsToSend;
   va_start(idsToSend, nbs);
 
-  for (int i = 0; i < nbs; ++i)
-  {
-    int id = va_arg ( idsToSend, int );
+  for (int i = 0; i < nbs; ++i) {
+    int id = va_arg(idsToSend, int);
     uint8_t bufferOut[MAX_MSG_LEN];
     char toSend[10];  // The biggest byte array in the nanopb encode seems to be 10 (should be coded to not depend on it tho...)
     pb_ostream_t stream = pb_ostream_from_buffer(bufferOut, sizeof(bufferOut));
     success = success && pb_encode(&stream, idToType[id], idToMsg[id]);
 
-    if(success)
-    {
+    if (success) {
       toSendBuilder += String(id);
       toSendBuilder += "|";
-      
-      for(int j = 0; j < stream.bytes_written; j++)
-      {
-        sprintf (toSend, "%02X", bufferOut[j]);
+
+      for (int j = 0; j < stream.bytes_written; j++) {
+        sprintf(toSend, "%02X", bufferOut[j]);
         toSendBuilder += String(toSend);
       }
     }
-    
+
     toSendBuilder += ";";
   }
   toSendBuilder += ">";
@@ -103,25 +95,22 @@ void PBUtils::pbSend(int nbs, ...)
  * 
  * @return: The number of submessages
  */
-int PBUtils::parseMsg(char inString[], int *msgIds, char **outStrings)
-{
+int PBUtils::parseMsg(char inString[], int *msgIds, char **outStrings) {
   // Split all messages
   char *wholeMsgs[MAX_MSG_LEN];
   int ind = 0;
-  char* substr = strtok(inString, ";");
+  char *substr = strtok(inString, ";");
 
-  while (substr != NULL)
-  {
+  while (substr != NULL) {
     wholeMsgs[ind++] = substr;
     substr = strtok(NULL, ";");
   }
 
   // Split (id, msg) pair
-  for (int i = 0; i < ind; i++)
-  {
-    char* id = strtok(wholeMsgs[i], "|");
-    char* msg = strtok(NULL, "|");
-    
+  for (int i = 0; i < ind; i++) {
+    char *id = strtok(wholeMsgs[i], "|");
+    char *msg = strtok(NULL, "|");
+
     msgIds[i] = atoi(id);
     outStrings[i] = msg;
   }
@@ -137,11 +126,10 @@ int PBUtils::parseMsg(char inString[], int *msgIds, char **outStrings)
  * 
  * @return: 
  */
-void PBUtils::charsToBytes(char* inString, uint8_t* stringValue)
-{
-  int len = strlen(inString)/2;
+void PBUtils::charsToBytes(char *inString, uint8_t *stringValue) {
+  int len = strlen(inString) / 2;
   for (int i = 0; i < len; ++i)
-    stringValue[i] = charToHex(inString[(i*2)], inString[(i*2)+1]);
+    stringValue[i] = charToHex(inString[(i * 2)], inString[(i * 2) + 1]);
 }
 
 /*
@@ -151,13 +139,11 @@ void PBUtils::charsToBytes(char* inString, uint8_t* stringValue)
  * 
  * @return: The decimal value
  */
-uint8_t PBUtils::charToHex(char in1, char in2)
-{
+uint8_t PBUtils::charToHex(char in1, char in2) {
   uint8_t val[2];
-  char in[2] = { in1, in2};
+  char in[2] = { in1, in2 };
 
-  for (int i = 0; i < 2; i++)
-  {
+  for (int i = 0; i < 2; i++) {
     if (in[i] >= 'A' && in[i] <= 'F')
       val[i] = in[i] - 'A' + 10;
     else if (in[i] >= 'a' && in[i] <= 'f')
@@ -165,6 +151,6 @@ uint8_t PBUtils::charToHex(char in1, char in2)
     else
       val[i] = in[i] - '0';
   }
- 
-  return val[0]*16 + val[1];
+
+  return val[0] * 16 + val[1];
 }
